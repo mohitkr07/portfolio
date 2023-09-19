1be7c3796aa4d8b47a61711e362908bb19d87656
Mohitkumar Mahto <2020kuec2031@iiitkota.ac.in>
2023-09-20 02:29:49 +0530

    projects added

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -3,11 +3,22 @@ import ProjectCard from "./utils/projectCart";
 
 const Projects = () => {
   const cityMarket = [
-    "Facilitated users in logging in, searching, viewing, rating, and adding products to the cart.",
+    "Facilitated users in authentication, searching, viewing, and adding products to the cart.",
     "Ensured seccure access by implementing robust user and seller authentication protocols.",
     "Enabled sellers to efficiently list & manage their products.",
     "Implemented advanced search and filtering.",
   ];
+  const weatherEx = [
+    "Integrated openweathermap API to get weather information.",
+    "Access current weather data for 200,000 + cities accross the globe.",
+    "Designed responsive and user friendly UI.",
+  ];
+  const lms = [
+    "Build web based application to manage multiple aspects of Educational Institutions.",
+    "Integrated Faculty Management System, Student Management System, Attendance Management System.",
+    "Integrated cheating proof quiz taking system",
+    "Designed and implemented responsive & interactive UI."
+  ]
   return (
     <>
       <div className={`${styles["projects"]} ${styles["page"]}`}>
@@ -21,8 +32,25 @@ const Projects = () => {
             title="City Market"
             belowTitle="Developed and deployed fully functional E-Commerce Platform"
             points={cityMarket}
+            github="https://github.com/mohitkr07/online-marketplace"
+            live="https://city-market-onlinemarket.onrender.com/"
           />
-          
+          <ProjectCard
+            imgLocation="projects/weather.png"
+            title="Weather-Ex"
+            belowTitle="Developed and deployed comprehensive Weather Application"
+            points={weatherEx}
+            github="https://github.com/mohitkr07/weather-app"
+            live="https://weather-ex.onrender.com/"
+          />
+          <ProjectCard
+            imgLocation="projects/lms.png"
+            title="Learning Management System"
+            belowTitle="Build comprehensive Institue Management system"
+            points={lms}
+            github="https://github.com/mohitkr07/Major-Project"
+            live="#"
+          />
         </div>
       </div>
     </>
--- src/components/utils/projectCart.jsx
+++ src/components/utils/projectCart.jsx
@@ -1,25 +1,45 @@
+import { useEffect, useState } from "react";
 import styles from "./utils.module.css";
 
 const ProjectCard = (props) => {
-
-    const points = props.points;
+  const points = props.points;
   return (
     <div className={styles["project-card"]}>
       <div className={styles["project-img"]}>
         <img src={props.imgLocation} />
       </div>
       <div className={styles["project-description"]}>
-        <p style={{fontSize: "250%", marginBottom: "5px"}} className={styles["neonText"]}>{props.title}</p>
-        <p style={{fontSize: "90%", fontStyle: "italic", marginBottom: "10px"}}>{props.belowTitle}</p>
-        <ul style={{fontSize: "88%"}}>
-          {points.map((i, key)=>{
-            return <li key={key}>{i}</li>
+        <div className={styles["title"]}>
+          <p
+            style={{ fontSize: "250%", marginBottom: "5px" }}
+            className={styles["neonText"]}
+          >
+            {props.title}
+          </p>
+          <span className={`${styles["redirect-link"]}`}>
+            <a href={props.github} target="_blank">
+              <i class="fa-brands fa-github"></i>
+            </a>
+
+            <a href={props.live} target="_blank">
+              <i className={`fa-solid fa-arrow-up-right-from-square`}></i>
+            </a>
+          </span>
+        </div>
+        <p
+          style={{ fontSize: "90%", fontStyle: "italic", marginBottom: "10px" }}
+        >
+          {props.belowTitle}
+        </p>
+        <ul style={{ fontSize: "88%" }}>
+          {points.map((i, key) => {
+            return <li key={key}>{i}</li>;
           })}
         </ul>
         <table>
-            <thead>
-                <td></td>
-            </thead>
+          <thead>
+            <td></td>
+          </thead>
         </table>
       </div>
     </div>
